010b299c52834c62bc76f4b406ff514d45c5d69d
Alexander Strey <alexander.strey@smail.th-koeln.de>
2025-11-25 17:15:52 +0100

    Main branch aufgeräumt. Eigene Exceptions für die Services hinzugefügt

---
+++ services/__init__.py
@@ -0,0 +1,4 @@
+from .tts_service import BarkTTSService
+from .llm_service import LLMService
+from .exceptions import TTSServiceError
+from .exceptions import LLMServiceError
---
+++ services/exceptions.py
@@ -0,0 +1,7 @@
+class TTSServiceError(Exception):
+    """Eigene Exception für Fehler im TTS Service"""
+    pass
+
+class LLMServiceError(Exception):
+    """Eigene Exception für Fehler im TTS Service"""
+    pass
