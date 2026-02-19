3ec49e843e8ac38dd6df7c19116468b58c571c72
Alexander Strey <alexander.strey@smail.th-koeln.de>
2026-02-19 11:20:35 +0100

    Config

--- repositories/__init__.py
+++
@@ -1,7 +0,0 @@
-from .base_repo import BaseRepo
-from .user_repo import UserRepo
-from .job_repo import JobRepo
-from .podcast_repo import PodcastRepo
-from .text_repo import TextRepo
-from .file_repo import FileRepo
-from .voice_repo import VoiceRepo
